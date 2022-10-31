e2fa61450b643b94b007b32d766bddb15c8390df
Sandeep Kandula <sandeep.kandula@thermofisher.com>
2022-10-31 17:27:07 +0530

    Cryptography Random Numbers.

---
+++ RandomNumber/RandomNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Security.Cryptography;
+
+Console.WriteLine("Random Number Demonstration in .NET 6");
+
+for (var i = 0; i < 10; i++)
+{
+    var randomNumbers = RandomNumberGenerator.GetBytes(32);
+    Console.WriteLine("Random Number "+ i + " : " 
+                      + Convert.ToBase64String(randomNumbers));
+}
+
+Console.ReadLine();
